21a1da6b54e06df7205738184a1b17ff8596b213
Jan van Hest <janvanhest@outlook.com>
2020-04-23 21:41:03 +0200

    Removed unneeded code from app.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,9 @@ import "./App.css";
 class App extends Component {
   foo = () => "bar";
   render() {
-    const name = "John Doe";
-    const loading = "false";
-    const showName = "true";
     return (
       <div className="App">
-        {loading ? <h4>Loading...</h4> : <h1>Hello {name}</h1>}
-        <h1>Hello from react - Github finder Project.</h1>
-        <h2>Hello {name}</h2>
-        <p>foo = {this.foo()}</p>
-        <p>1 + 1 = {1 + 1}</p>
-        <p>should i show the name?:{showName && name}</p>
+        <h1>Hello</h1>
       </div>
     );
   }
