0145c5a91b131a12f2df682071fad93b1e915e59
Jan van Hest <janvanhest@outlook.com>
2020-05-03 20:30:03 +0200

    Added Home and not found componet.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Alert from "./components/layout/Alert";
 import Navbar from "./components/layout/Navbar";
-import Search from "./components/users/Search";
-import Users from "./components/users/Users";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 import User from "./components/users/User";
 import About from "./components/pages/About";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
 
 const App = () => {
-  // const [users, setUsers] = useState([]);
-  // const [user, setUser] = useState({});
-  // const [alert, setAlert] = useState(null);
-
-  // async componentDidMount() {
-  //   console.log(process.env.REACT_APP_CLIENT_SECRET);
-  //   this.setState({ loading: true });
-  //   const res = await axios.get(
-  //     `https:/api.github.com/users?client_id=
-  //     ${process.env.REACT_APP_GITHUB_CLIENT_ID}
-  //     &client_secret=
-  //     ${process.env.REACT_APP_CLIENT_SECRET}`
-  //   );
-
-  //   setUser(res.data.items)
-  //   setLoading(false)
-  // }
-
   return (
     <GithubState>
       <AlertState>
@@ -38,18 +20,10 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
